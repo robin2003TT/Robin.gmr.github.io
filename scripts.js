de49c10947ee3bb651111009bddad33fd3b47afd
robin2003TT <huzaiaprogmr@gmail.com>
2025-03-09 19:50:12 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Music Controls
+let music = document.getElementById("bg-music");
+let musicBtn = document.getElementById("music-btn");
+let audioSource = document.getElementById("audio-source");
+let musicName = document.getElementById("music-name");
+
+// List of songs (Update song names as needed)
+let songs = [
+    { name: "Showreel", file: "showreel.mp3" },
+    { name: "Joy", file: "joy.mp3" },
+    { name: "Royalty", file
